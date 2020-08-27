e906c33a7cc2bad5a645b158cce14633d971e34e
Stephen Tallamy <stephen.tallamy@gmail.com>
2020-08-27 23:24:07 +0100

    Better handling of no loaded instrument

--- kontakt/generator.lua
+++ kontakt/generator.lua
@@ -33,14 +33,6 @@ local layer_map = {
 }
 
 local path = scriptPath .. filesystem.preferred("/samples/")
-print("The samples are located in " .. path)
-
--- Check for valid instrument.
-if not instrument then
-    print("The following error message informs you that the Creator Tools are not "..
-          "focused on a Kontakt instrument. To solve this, load an instrument in "..
-          "Kontakt and select it from the instrument dropdown menu on top.")
-end
 
 function create_group(groups, i, name)
     local group
@@ -84,76 +76,90 @@ function setup_group(group, file, layer_info)
         root = root + note_interval
         bar = bar + note_duration_bars
     end
-end   
+end  
 
--- Declare an empty table which we will fill with the samples.
-local samples = {}
+function process_samples()
 
-local max_rr = 0
-for _,p in filesystem.directoryRecursive(path) do
-    if filesystem.isRegularFile(p) then
-      if filesystem.extension(p) == '.wav' or filesystem.extension(p) == '.aif' or filesystem.extension(p) == '.aiff' then
-        local filename = filesystem.filename(p)
-        local file_no_ext = filename:match("(.+)%..+")
-        local i = 0
-        local zone_name
-        local rr = 1
-        for part in file_no_ext:gmatch("%S+")do
-          if i == 0 then
-            zone_name = part
-          elseif i == 1 then
-            rr = tonumber(string.match(part, '%d'))
-            if rr > max_rr then
-                max_rr = rr
+    -- Declare an empty table which we will fill with the samples.
+    local samples = {}
+
+    local max_rr = 0
+    for _,p in filesystem.directoryRecursive(path) do
+        if filesystem.isRegularFile(p) then
+        if filesystem.extension(p) == '.wav' or filesystem.extension(p) == '.aif' or filesystem.extension(p) == '.aiff' then
+            local filename = filesystem.filename(p)
+            local file_no_ext = filename:match("(.+)%..+")
+            local i = 0
+            local zone_name
+            local rr = 1
+            for part in file_no_ext:gmatch("%S+")do
+            if i == 0 then
+                zone_name = part
+            elseif i == 1 then
+                rr = tonumber(string.match(part, '%d'))
+                if rr > max_rr then
+                    max_rr = rr
+                end
             end
-          end
-          i = i + 1
+            i = i + 1
+            end
+            if samples[zone_name] == nil then
+                samples[zone_name] = {}
+            end
+            samples[zone_name][rr] = filename
         end
-        if samples[zone_name] == nil then
-            samples[zone_name] = {}
         end
-        samples[zone_name][rr] = filename
-      end
     end
-end
 
--- Reset the instrument groups.
-instrument.groups:reset()
+    -- Reset the instrument groups.
+    instrument.groups:reset()
 
-local groups = {}
-for i=0,max_rr-1 do
-    local group_name = 'RR'..(i+1)
-    create_group(groups, i, group_name)
-end
+    local groups = {}
+    for i=0,max_rr-1 do
+        local group_name = 'RR'..(i+1)
+        create_group(groups, i, group_name)
+    end
 
-create_group(groups, i, 'RT')
+    create_group(groups, i, 'RT')
 
-for zone,v in pairs(samples) do
-    if (zone == 'RT') then
-        for _,file in pairs(v) do
-            setup_group(groups['RT'], file, layer_map['RT'])   
-        end   
-    else 
-        local num_rr = 0
-        local last_file = nil
-        for i,file in pairs(v) do
-            local group_name = 'RR'..i
-            local layer = layer_map[zone]
-            if layer ~= nil then
-                setup_group(groups[group_name], file, layer)
-                num_rr = num_rr + 1
-            end
-            last_file = file
-        end
-        if num_rr < max_rr then
-            print('Missing RR for '..zone..' duplicating with '..last_file)
-            for i=num_rr+1,max_rr do
+    for zone,v in pairs(samples) do
+        if (zone == 'RT') then
+            for _,file in pairs(v) do
+                setup_group(groups['RT'], file, layer_map['RT'])   
+            end   
+        else 
+            local num_rr = 0
+            local last_file = nil
+            for i,file in pairs(v) do
                 local group_name = 'RR'..i
                 local layer = layer_map[zone]
                 if layer ~= nil then
-                    setup_group(groups[group_name], last_file, layer)
+                    setup_group(groups[group_name], file, layer)
+                    num_rr = num_rr + 1
+                end
+                last_file = file
+            end
+            if num_rr < max_rr then
+                print('Missing RR for '..zone..' duplicating with '..last_file)
+                for i=num_rr+1,max_rr do
+                    local group_name = 'RR'..i
+                    local layer = layer_map[zone]
+                    if layer ~= nil then
+                        setup_group(groups[group_name], last_file, layer)
+                    end
                 end
             end
-        end
-    end   
+        end   
+    end
+end
+
+print("The samples are located in " .. path)
+
+-- Check for valid instrument.
+if not instrument then
+    print("The following error message informs you that the Creator Tools are not "..
+          "focused on a Kontakt instrument. To solve this, load an instrument in "..
+          "Kontakt and select it from the instrument dropdown menu on top.")
+else 
+    process_samples()
 end
